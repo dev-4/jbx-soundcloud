cdb80f31f0f48d6d775fa59d813a429058de9bfa
dev-4 <dev.4@protonmail.com>
2016-09-23 17:04:04 -0400

    Added artwork_url and title, fixed some stuff

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+var tracks;
+var currentSong = 0; //increase currentSong by 0
+
 $(document).ready(function(){
 	SC.initialize({
  	 client_id: 'f77a15a6adfbb5035c8a8cf38e399f1e'
@@ -17,16 +20,21 @@ $(document).ready(function(){
 function scSearch(term) {
 	SC.get('/tracks', {
   q: term
-}).then(function(tracks) {
-	SC.stream( '/tracks/' + tracks[0].id ).then(function(player){    
+}).then(function(response) {
+	tracks = response;
+	playNext();
+});
+};
+function playNext() {
+SC.stream( '/tracks/' + tracks[currentSong].id ).then(function(player){    
 		console.log(player); 
 		console.log(tracks);   
 		player.play();
+		$("#artwork").attr("src", tracks[currentSong].artwork_url);
+		$("#title").html("Now playing: " + tracks[currentSong].title);
 		player.on("finish",function(){
       currentSong += 1; //increase currentSong by 1
       playNext();       //call itself
     });
 	});
-});
 };
-
