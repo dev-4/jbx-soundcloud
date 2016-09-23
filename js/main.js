c327fcf5b4d904f861a77e2a4e1fd34fdc40bdda
dev-4 <dev.4@protonmail.com>
2016-09-23 15:58:40 -0400

    Fixed the goddamn player to make it play

--- js/main.js
+++ js/main.js
@@ -1,33 +1,32 @@
 $(document).ready(function(){
 	SC.initialize({
-  client_id: 'f77a15a6adfbb5035c8a8cf38e399f1e'
-}); // client_id
-  // SC.get("/tracks",{
-  //   q: term
-  //   }).then(function(response) {
-  //   // things to do after the tracks load...
-  //   console.log( response );
-  // });
-$("#smit").click(function() {
-		console.log("YOU CLICKED");
-	});
-
+ 	 client_id: 'f77a15a6adfbb5035c8a8cf38e399f1e'
+	}); // client_id
 	$("#search_form").submit(function(event){
 		event.preventDefault();
 		// Clear the Search results
 		$("#songs").html("");
 		// Search for the submitted term
-		omdbSearch( $("#search").val() );
+		scSearch( $("#search").val() );
 
 		console.log($("#search").val() );
 		// $(...).val() gets us
 		// element.value
 	});
 }); // $(document).ready
-function omdbSearch(term) {
+function scSearch(term) {
 	SC.get('/tracks', {
-  q: term, license: 'cc-by-sa'
+  q: term
 }).then(function(tracks) {
-  console.log(tracks);
+	SC.stream( '/tracks/' + tracks[0].id ).then(function(player){    
+		console.log(player); 
+		console.log(tracks);   
+		player.play();
+		player.on("finish",function(){
+      currentSong += 1; //increase currentSong by 1
+      playNext();       //call itself
+    });
+	});
 });
 };
+
